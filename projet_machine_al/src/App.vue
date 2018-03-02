e6a36079ea5493081455365dcb8d5e516bbf70a6
Aurélie Lafranceschina <a.lafranceschina@Digital-Grenoble.local>
2018-03-02 13:34:56 +0100

    affichage machines +logo

--- projet_machine_al/src/App.vue
+++ projet_machine_al/src/App.vue
@@ -1,21 +1,23 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img src="./assets/coffee_logo.png">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <h2>{{machines.name}}</h2>
+
+    <toggle-button :value="false" 
+               color="#82C7EB" 
+               :sync="true" 
+               :labels="true"/>
+
+    
+    <h2>Machine 2</h2>
+
+    <toggle-button :value="false" 
+               color="#82C7EB" 
+               :sync="true" 
+               :labels="true"/>
+
+    
   </div>
 </template>
 
@@ -24,7 +26,20 @@ export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Machines à café',
+
+      machines: [{
+        name:'Machine 1',
+        etat:'Machine 2'
+      },
+      {
+        name:'Machine 1',
+        etat:'Machine 2'
+      }]
+
+
+
+
     }
   }
 }
--- projet_machine_al/src/main.js
+++ projet_machine_al/src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ToggleButton from 'vue-js-toggle-button'
+Vue.use(ToggleButton)
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(App),
+
+
+    
+  
 })
