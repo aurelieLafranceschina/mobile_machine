05b83502b7677d73dfd6ab9b7cde895e0cb7abb6
Aurélie Lafranceschina <a.lafranceschina@Digital-Grenoble.local>
2018-03-02 09:52:14 +0100

    new project

---
+++ projetApp1/js.js
@@ -0,0 +1,26 @@
+
+
+new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue.js!',      
+
+      titre1:'',
+
+      titre2:'',
+
+      active: false,
+
+      checked: 'Toggle red class',
+
+      show: false,
+
+      update: ''
+
+    },
+
+    
+
+  })
+
+ 
